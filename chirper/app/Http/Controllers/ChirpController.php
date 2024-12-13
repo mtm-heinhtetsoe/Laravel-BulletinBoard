67a1cba32db8cbbd4e81cf15fb0e58667314ac51
mtm-heinhtetsoe <heinhtetsoe@metateammyanmar.com>
2024-12-13 15:29:01 +0630

    chirp init

---
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps.index');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
+    }
+}
--- chirper/routes/web.php
+++ chirper/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps');
+Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
